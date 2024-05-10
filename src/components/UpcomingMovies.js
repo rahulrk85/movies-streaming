1b90b7ee306f7733bcb99d98626cfa516e6ee255
Rahul Kulkarni <rahulrkulkarni20@gmail.com>
2024-05-10 11:32:55 +0530

    not able to filter the posters

--- src/components/UpcomingMovies.js
+++ src/components/UpcomingMovies.js
@@ -6,32 +6,45 @@ const UpcomingMovies = () => {
 
   const [movies,setmovies] = useState();
 
-  useEffect(()=>{FetchUpcomingMovies()},[]);
+  useEffect(()=>{FetchUpcomingMovies1()},[]);
 
 
-    const FetchUpcomingMovies = async() =>{
-      const url = 'https://imdb188.p.rapidapi.com/api/v1/getUpcomingMovies?region=US';
-      const options = {
-	      method: 'GET',
-	      headers: {
-		      'X-RapidAPI-Key': '7e321a2e3dmshf92e1f9626cf012p1bbd6ejsn442803ed61e7',
-		      'X-RapidAPI-Host': 'imdb188.p.rapidapi.com'
-	      }
-      };
+  const FetchUpcomingMovies1 = async() =>{
+    const data = await fetch("https://sidecar.tvtime.com/sidecar/tvtcached?o=https://discover.tvtime.com/v1/discover/cgw/trending");
+    const json = await data.json();
+    console.log(json.data);
+    setmovies(json.data);
+  }
+    // const FetchUpcomingMovies = async() =>{
+    //   const url = 'https://imdb188.p.rapidapi.com/api/v1/getUpcomingMovies?region=US';
+    //   const options = {
+	  //     method: 'GET',
+	  //     headers: {
+		//       'X-RapidAPI-Key': '7e321a2e3dmshf92e1f9626cf012p1bbd6ejsn442803ed61e7',
+		//       'X-RapidAPI-Host': 'imdb188.p.rapidapi.com'
+	  //     }
+    //   };
 
-    try {
-	    const response = await fetch(url, options);
-	    const result = await response.json();
-      setmovies(result?.message);
-	    // console.log(result.message);
+    // try {
+	  //   const response = await fetch(url, options);
+	  //   const result = await response.json();
+    //   setmovies(result?.message);
+	  //   // console.log(result.message);
 
-    } catch (error) {
-	    console.error(error);
-    }
-    }
+    // } catch (error) {
+	  //   console.error(error);
+    // }
+    // }
     if(!movies) return <Shimmer />;
 
-    console.log(movies);
+    const filterPoster = movies.filter((item)=>(item.posters[0].url || item.poster.url));
+    console.log(filterPoster);
+
+
+
+
+    
+    // console.log(movies);
   return (
     <div>
       <h1 className='text-6xl font-extrabold mx-20 my-3 p-2 font-serif'>Up Coming Movies</h1>
